aba304c80a0c3c25e3193fef2404001d761dbeb0
AibulatKhusnut <140543469+AibulatKhusnut@users.noreply.github.com>
2023-07-25 23:44:55 +0500

    Python_video_on_audio

--- 1.py
+++ 1.py
@@ -1,5 +1,7 @@
 import moviepy.editor
+from pathlib import Path
 
-video = moviepy.editor.VideoFileClip('VID-20220703-WA0182.mp4')
+video_file = Path('VID-20220703-WA0182.mp4')
+video = moviepy.editor.VideoFileClip(f'{video_file}')
 audio = video.audio
-audio.write_audiofile('VID-20220703-WA0182.mp3')
+audio.write_audiofile(f'{video_file.stem}.stem.mp3')
